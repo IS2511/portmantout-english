8a53ead20429da6228b2a91472dda810e6ad4395
dodic <dodickgod@gmail.com>
2022-07-20 23:18:48 +0300

    More idiomatic (also for learn) code style

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,77 @@
-use std::fmt;
-use std::io::{BufRead, Read};
+use cast::u8;
 use rand::Rng;
+use std::{
+    borrow::Cow,
+    convert::Infallible,
+    error::Error,
+    fmt,
+    fs::File,
+    io::{self, BufRead, BufReader, Read},
+};
+use tap::Pipe;
 
-// #[derive(Debug, Clone)]
-// struct PmtWord {
-//     word: String,
-//     end_at: u8,
-// }
-//
-// impl PmtWord {
-//     pub fn new(word: String, cut: u8) -> Result<Self, &'static str> {
-//         if (cut as usize) > word.len() {
-//             return Err("Cut point too far, word too short")
-//         }
-//         if cut == 0 {
-//             return Err("Cut cannot be zero");
-//         }
-//         Ok(PmtWord { word, end_at: cut })
-//     }
-//     // pub fn word(&self) -> String {
-//     //     self.word.clone()
-//     // }
-//     pub fn cut(&self) -> Option<String> {
-//         let word_cut = self.word.get(..(self.end_at as usize));
-//         if word_cut.is_some() {
-//             return Some(String::from(word_cut.unwrap()));
-//         }
-//         None
-//     }
-// }
-//
-// impl fmt::Display for PmtWord {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(f, "{}", self.word.get(..(self.end_at as usize))
-//             .expect("Cut point too far, word too short"))
-//     }
-// }
+#[derive(Debug, Clone)]
+struct PmtWord<'a> {
+    word: Cow<'a, str>,
+    end_at: u8,
+}
+
+impl<'a> PmtWord<'a> {
+    pub fn new(word: impl Into<Cow<'a, str>>, cut: u8) -> Result<Self, &'static str> {
+        let word = word.into();
+        if (cut as usize) > word.len() {
+            return Err("Cut point too far, word too short");
+        }
+        if cut == 0 {
+            return Err("Cut cannot be zero");
+        }
+        Ok(PmtWord { word, end_at: cut })
+    }
+    // pub fn word(&self) -> String {
+    //     self.word.clone()
+    // }
+    pub fn cut(&self) -> Option<&str> {
+        self.word.get(..(self.end_at as usize))
+    }
+}
+
+impl fmt::Display for PmtWord<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", &self.word[..(self.end_at as usize)])
+    }
+}
 
 // struct PmtFull<'a> {
 //     words: Vec<PmtWord<'a>>,
 // }
-
+//
 // impl PmtFull {
 //     pub fn new() -> Self {
 //         PmtFull { words: Vec::new() }
 //     }
+
+// pub fn push(&mut self, elem: PmtWord) {
 //
-//     // pub fn push(&mut self, elem: PmtWord) {
-//     //
-//     // }
+// }
 // }
 // type PmtFull<'a> = Vec<PmtWord<'a>>;
 
 #[derive(Debug, Clone)]
 struct PmtTreeWord<'a> {
-    prev_word_cut: u8, // previous -> current
-    word: &'a String,
-    word_end: Vec<PmtTreeWord<'a>>
+    prev_word_cut: u8,
+    // previous -> current
+    // Possible use `Cow<'a, str>`
+    word: &'a str,
+    word_end: Vec<PmtTreeWord<'a>>,
+}
+
+impl Iterator for PmtTreeWord<'_> {
+    // later use never type !
+    type Item = Infallible;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!("please implement `Iterator`")
+    }
 }
 
 // 01234567   0123456
@@ -65,113 +80,128 @@ struct PmtTreeWord<'a> {
 // |----| <- prev_word_cut
 // ^^^^^^^^ <- word_end[123]
 
-impl PmtTreeWord {
-    pub fn new_root(word: &String) -> Self {
+impl<'a> PmtTreeWord<'a> {
+    pub fn new_root(word: &'a str) -> Self {
         PmtTreeWord {
             prev_word_cut: 0,
             word,
-            word_end: Vec::new()
+            word_end: Vec::new(),
         }
     }
 
-    pub fn push(&self, word: &String, prev_word_cut: u8) -> Result<&Self, &'static str> {
+    pub fn push(&self, _: &str, prev_word_cut: u8) -> Result<&PmtTreeWord, &'static str> {
         if (prev_word_cut as usize) >= self.word.len() {
-            return Err("cut point too far, previous word too short")
+            return Err("cut point too far, previous word too short");
+        } else {
+            Ok(self)
         }
-        Ok(self)
     }
 
     // Find and return the PmtTreeWord that contains the given word, traversing the tree
-    pub fn find_word(&self, word: &String) -> Option<&Self> {
-        if self.word == word {
-            return Some(self)
-        }
-        for sub_word in &self.word_end {
-            let sub_find = sub_word.find_word(word);
-            if sub_find.is_some() {
-                return sub_find;
-            }
-        }
-        None
-    }
-}
-
-fn build_pmt_from_iterator(mut words: impl Iterator<Item = std::io::Result<String>>) -> Vec<PmtWord> {
-    let mut pmt: Vec<PmtWord> = Vec::new();
-
-    // let worker = || {
-    //
-    // }
-    let mut words_empty = false;
-    while !words_empty {
-        let word = words.next();
-        if word.is_some() {
-            let word_read = word.unwrap();
-            if word_read.is_ok() {
-                let word_ok = word_read.unwrap();
-                if word_ok.len() < 256 {
-                    pmt.push(PmtWord::new(word_ok.clone(), word_ok.len() as u8).unwrap());
-                }
-            }
+    pub fn find_word(&self, word: &str) -> Option<&PmtTreeWord> {
+        if self.word != word {
+            self.word_end.iter().find_map(|sub| sub.find_word(word))
         } else {
-            words_empty = true;
+            Some(self)
         }
     }
-
-    return pmt;
 }
 
-fn build_pmt_from_static(words: Vec<String>) -> PmtTreeWord {
-    let mut pmt: PmtTreeWord = PmtTreeWord::new_root(&words[rand::thread_rng().gen_range(0, words.len())]);
-
-    for word in words {
-        let pmt_word = PmtTreeWord::new();
+fn build_pmt_from_iterator(
+    words: impl Iterator<Item = io::Result<String>>,
+) -> io::Result<Vec<PmtWord<'static>>> {
+    // is nightly feature + my ext for infer type in `stable_try`
+    fn other_box(error: Box<dyn Error + Send + Sync>) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, error)
     }
 
-    // for word in words {
-    //     let pmt_word = PmtWord::new(word.clone(), word.len() as u8);
-    //     if pmt_word.is_ok() {
-    //         pmt.push(pmt_word.unwrap());
-    //     }
-    // }
+    // stable try block
+    macro_rules! stable_try {
+        ($($t:tt)*) => {
+            (|| {
+                $($t)*
+            })()
+        };
+    }
 
-    return pmt;
+    // Unlike your implementation it does not skip elements but returns an error
+    // if you want old behavior, use `.flat_map`
+    words
+        .map(|word| {
+            word.and_then(|word| {
+                stable_try! {
+                    u8(word.len())?
+                        .pipe(|len| PmtWord::new(word, len))?
+                        .pipe(Ok)
+                }
+                .map_err(other_box)
+            })
+        })
+        .collect()
+}
+
+fn build_pmt_from_static(words: Vec<String>) -> PmtTreeWord<'static> {
+    // let mut pmt: PmtTreeWord =
+    //     PmtTreeWord::new_root(&words[rand::thread_rng().gen_range(0, words.len())]);
+    //
+    // for word in words {
+    //     let pmt_word = PmtTreeWord::new();
+    // }
+    //
+    // // for word in words {
+    // //     let pmt_word = PmtWord::new(word.clone(), word.len() as u8);
+    // //     if pmt_word.is_ok() {
+    // //         pmt.push(pmt_word.unwrap());
+    // //     }
+    // // }
+    //
+    // return pmt;
+    todo!()
 }
 
 fn print_help(this_name: &str) {
-    println!("Usage: {} <dict-file> [-h|--help]", this_name);
+    println!("Usage: {this_name} <dict-file> [-h|--help]");
 }
 
-fn main() {
+// fs can return error, don't ignore it
+fn main() -> io::Result<()> {
     let args: Vec<_> = std::env::args().collect();
 
     for arg in args.iter() {
         if (arg == "--help") || (arg == "-h") {
             print_help(&args[0]);
-            return;
+            todo!("handle arg parsing")
         }
     }
     if args.len() < 2 {
         print_help(&args[0]);
-        return;
+        todo!("handle arg parsing")
     }
 
-    let dict_filename: &str = &args[1];
-    let dict_file = std::fs::File::open(dict_filename)
-        .expect("Couldn't open file.");
-    let reader = std::io::BufReader::new(dict_file);
+    let dict_filename = &args[1];
+    // now instead of
+    let dict_file = File::open(dict_filename).expect("Couldn't open file.");
+    // you can write
+    // let dict_file = File::open(dict_filename)?;
+    let reader = BufReader::new(dict_file);
 
     // let words: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    // your:
     let mut words: Vec<String> = Vec::new();
     for line in reader.lines() {
-        if line.is_ok() {
-            words.push(line.unwrap());
+        if let Ok(line) = line {
+            words.push(line);
         }
     }
+    // suggest:
+    // let words: io::Result<Vec<_>> = reader.lines().collect();
+
     let pmt = build_pmt_from_static(words);
 
     for word in pmt {
-        print!("{} ", word);
+        print!("{word} ");
     }
     println!();
+
+    Ok(())
 }
